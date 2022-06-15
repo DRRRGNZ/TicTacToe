f76b756c5e88d4e110537ca54780be469bbf59c6
Praktika1 <praktika1@connext.de>
2022-06-15 14:10:26 +0200

    KI gegen KI

--- tictactoescript.js
+++ tictactoescript.js
@@ -18,8 +18,7 @@ document.addEventListener('DOMContentLoaded', Spielfeldgenerieren)
 function buttongeklickt(element) {   // 0
     var button = element.target;
     if (current === 0) {
-        button.setAttribute("class", "spieler0")
-        button.innerText = "X";
+        gegenspieler()
 
     } else {
         button.setAttribute("class", "spieler1")
@@ -34,6 +33,26 @@ function buttongeklickt(element) {   // 0
     current = 1 - current// 1
     button.setAttribute("disabled", "true")
 }
+
+function KIgegenKI() {
+    for (var h = 0; h < 1;) {
+        if (current === 0) {
+            gegenspieler()
+        }
+
+        const myTimeout = setTimeout(minimax, 3000)
+        minimax();
+    }
+}
+
+function minimax() {
+    if (wergewinnt2() !== true && SpielegegenKI === true) {
+
+        felderchecken()
+        wergewinnt()
+    }
+}
+
 
 function wergewinnt() {
 
@@ -78,6 +97,23 @@ function spielende() {
     Gewinneranzeige();
 }
 
+function wergewinnt2() {
+
+    var allebuttons = document.querySelectorAll(' td > button')
+
+    var cAs = "spieler" + current
+
+    for (var k = 0; k < 8; k++) {
+        if (allebuttons[gewinnBedingungen[k][0]].getAttribute('class') === cAs
+            && allebuttons[gewinnBedingungen[k][1]].getAttribute('class') === cAs
+            && allebuttons[gewinnBedingungen[k][2]].getAttribute('class') === cAs) {
+            return true;
+        }
+    }
+    return unentschieden()
+}
+
+
 function Gewinneranzeige() {
     alert("Spieler " + current + " gewinnt!")
 }
@@ -106,6 +142,90 @@ function Spielfeldgenerieren() {
         current = 0;
     }
 
+}
+
+function Spielfeldgenerieren2() {
+
+    var dialog = document.getElementById("dialog")
+
+    document.querySelector('table')?.remove()
+    var neueTabelle = document.createElement('table')
+    var div = document.getElementById('t1')
+    div.appendChild(neueTabelle)
+
+    for (k = 0; k < 3; k++) {
+        var zeilenbruch = document.createElement('tr')
+        for (i = 0; i < 3; i++) {
+            var button = document.createElement('button')
+            button.addEventListener('click', buttongeklickt)
+            var td = document.createElement('td')
+            td.appendChild(button)
+            zeilenbruch.appendChild(td)
+        }
+        neueTabelle.appendChild(zeilenbruch)
+        current = 0;
+    }
+
+}
+
+function gegenspieler() {
+    if (kannKIGewinnen("spieler1") === false) {
+        if (kannKIGewinnen("spieler0") === false) {
+            zufaelligesFeld()
+        }
+    }
+}
+
+function kannKIGewinnen(spieler) {
+    var allebuttons = document.querySelectorAll('td >button')
+    for (var x = 0; x < 8; x++) {
+        if (allebuttons[gewinnBedingungen[x][0]].getAttribute('class') === spieler &&
+            allebuttons[gewinnBedingungen[x][1]].getAttribute('class') === spieler &&
+            allebuttons[gewinnBedingungen[x][2]].getAttribute('class') === null) {
+            // KI kann gewinnen
+            allebuttons[gewinnBedingungen[x][2]].setAttribute("class", "spieler0")
+            allebuttons[gewinnBedingungen[x][2]].innerText = "X";
+            current = 1 - current
+            allebuttons[gewinnBedingungen[x][2]].setAttribute("disabled", "true")
+            return true;
+        } else if (allebuttons[gewinnBedingungen[x][0]].getAttribute('class') === spieler &&
+            allebuttons[gewinnBedingungen[x][1]].getAttribute('class') === null &&
+            allebuttons[gewinnBedingungen[x][2]].getAttribute('class') === spieler) {
+            // KI kann gewinnen
+            allebuttons[gewinnBedingungen[x][1]].setAttribute("class", "spieler0")
+            allebuttons[gewinnBedingungen[x][1]].innerText = "X";
+            current = 1 - current
+            allebuttons[gewinnBedingungen[x][1]].setAttribute("disabled", "true")
+            return true;
+        } else if (allebuttons[gewinnBedingungen[x][0]].getAttribute('class') === null &&
+            allebuttons[gewinnBedingungen[x][1]].getAttribute('class') === spieler &&
+            allebuttons[gewinnBedingungen[x][2]].getAttribute('class') === spieler) {
+            // KI kann gewinnen
+            allebuttons[gewinnBedingungen[x][0]].setAttribute("class", "spieler0")
+            allebuttons[gewinnBedingungen[x][0]].innerText = "X";
+            current = 1 - current
+            allebuttons[gewinnBedingungen[x][0]].setAttribute("disabled", "true")
+            return true;
+        }
+    }
+    return false;
+}
+
+function zufaelligesFeld() {
+
+    var allebuttons = document.querySelectorAll(' td > button')
+    var s1 = "spieler" + 0
+    var s2 = "spieler" + 1
+    var zufaelligeZahl = Math.floor(Math.random() * 8)
+    if (allebuttons[zufaelligeZahl].getAttribute('class') === s1 ||
+        allebuttons[zufaelligeZahl].getAttribute('class') === s2) {
+        zufaelligesFeld()
+    } else {
+        allebuttons[zufaelligeZahl].setAttribute("class", "spieler0")
+        allebuttons[zufaelligeZahl].innerText = "X";
+        current = 1 - current
+        allebuttons[zufaelligeZahl].setAttribute("disabled", "true")
+    }
 }
 
 function spielmodusaussuchen(SpielgegenKI) {
@@ -216,4 +336,4 @@ function unentschieden2() {
 
 /*
 
- */
+*/
